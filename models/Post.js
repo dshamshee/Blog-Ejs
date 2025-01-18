5790fe8c0737bf96138557f1aba3eeea7990d919
Danish <danishshamshee123@gmail.com>
2025-01-18 22:08:41 +0530

    like, delete feature implemented

--- models/Post.js
+++ models/Post.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 const postSchema = new mongoose.Schema({
     title: { type: String, required: true },
     body: { type: String, required: true },
+    caption: { type: String, required: true },
     image: {type: String, default: "https://images.pexels.com/photos/2101894/pexels-photo-2101894.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
-    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
     username: String,
     createdAt: { type: Date, default: Date.now },
-    like: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }],
     // categories: [String],
     // tags: [String],
 });
--- routes/posts.js
+++ routes/posts.js
@@ -8,6 +8,7 @@ const blogUploads = require('../config/post_multer_config');
 
 
 
+
 router.get('/create/:userid',isLoggedin, async function(req, res){
     const user = req.user;
     res.render('posts/create', {user});
@@ -16,18 +17,59 @@ router.get('/create/:userid',isLoggedin, async function(req, res){
 
 router.post('/create',blogUploads.single('image'), isLoggedin, async function(req, res){
     const user = req.user;
-    const {title, body} = req.body;
+    const {title, caption, body} = req.body;
     const post = await postModel.create({
         title,
+        caption,
         body,
         image: req.file.filename,
         author: user._id,
         username: user.username
     });
-    console.log("First Blog Post Successfuly!");
+    res.redirect('/');
+});
+
+
+router.get('/view/:id',isLoggedin,async function(req, res){
+    const id = req.params.id;
+    const postID = req.params.id;
+    const post = await postModel.findById({_id: postID});
+    const user = req.user;
+    res.render('posts/viewPost',{user, post});
+});
+
+
+router.get('/like/:id',isLoggedin, async function(req, res){
+    const post = await postModel.findById({_id: req.params.id}).populate('author');
+
+    if(post.likes.indexOf(req.user._id)=== -1 ){
+        post.likes.push(req.user._id); // Add the user's id to the likes array
+    }
+    else{
+        post.likes.splice(post.likes.indexOf(req.user._id), 1);
+    }
+    await post.save();
     res.redirect('/');
 })
 
+router.get('/profile/like/:id',isLoggedin, async function(req, res){
+    const post = await postModel.findById({_id: req.params.id}).populate('author');
 
+    if(post.likes.indexOf(req.user._id)=== -1 ){
+        post.likes.push(req.user._id); // Add the user's id to the likes array
+    }
+    else{
+        post.likes.splice(post.likes.indexOf(req.user._id), 1);
+    }
+    await post.save();
+    res.redirect('/user/profile');
+})
+
+
+router.get('/delete/:id',async function(req, res){
+    await postModel.deleteOne({_id: req.params.id});
+    console.log("Post deleted successfully!");
+    res.redirect('/user/profile');
+})
 
 module.exports = router;
