5fe7c0fa45afacda9b0b24a76bac993fff3a2d00
Danish <danishshamshee123@gmail.com>
2025-01-21 12:52:55 +0530

    final changes of tinymce and file uploads

--- config/post_multer_config.js
+++ config/post_multer_config.js
@@ -1,8 +1,12 @@
 const multer = require('multer'); // Import multer
 const path = require('path'); // Import path 
 const crypto = require('crypto'); // Import crypto (It is used to generate a random hash for the uploaded file)
+const fs = require('fs');
+const uploadDir = './public/images/blogUploads';
 
-
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 // diskstorage 
 // Multer settings (It is used to accept file uploads) and save the file into the give destination by given name
@@ -14,6 +18,7 @@ const storage = multer.diskStorage({
         // Generating a random hash for the uploaded file and concatinating with the file extension
         crypto.randomBytes(12, function(err, name){
            const fn = name.toString("hex")+path.extname(file.originalname);
+           console.log('Processing file:', file.originalname); // Debug log
             cb(null, fn);
         })
     }
