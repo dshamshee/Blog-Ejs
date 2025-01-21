38d50e1c14baec2b85a83a8e4a665cb7481d9a0e
Danish <danishshamshee123@gmail.com>
2025-01-21 13:27:57 +0530

    profile update

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name: {type: String, required: true, unique: true},
     username: {type: String, required: true, unique: true},
-    bio: {type: String, default: 'Nothing'},
+    bio: {type: String, default: 'Bio not set'},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     role: { type: String, enum: ['user', 'admin'], default: 'user' },
